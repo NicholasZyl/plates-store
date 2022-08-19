7035af0914d73c672e1fb54bb290719fccc363e4
Mikołaj Żyłkowski <mzylkowski@gmail.com>
2022-08-19 12:30:34 +0200

    test: Add additional test for the simple case

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -1,8 +1,13 @@
 import requests
 
 
+def get_api_url() -> str:
+    return "http://localhost:8080/"
+
+
 def test_api_returns_empty_list_when_no_plates():
-    url = "http://localhost:8080/plate"
+    url = f"{get_api_url()}/plate"
     request = requests.get(url)
 
     assert request.status_code == 200
+    assert request.json() == []
